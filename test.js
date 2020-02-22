d76b25bd1b332afc0b35ce89d1b76a7fd28c6d40
Colin Fahy <colinfahy@spotify.com>
2020-02-22 13:35:43 -0500

    get devices in realtime

--- test.js
+++ test.js
@@ -1,9 +1,45 @@
 const player = require('./lib/player.js');
 
-const deviceName = process.argv[2];
-const play = process.argv[3];
+const [, , cmd, ...args] = process.argv;
 
-player
-  .playOnDevice(deviceName, play)
-  .then(() => console.log('Playback started successfully'))
-  .catch((err) => { console.error(err); });
+
+switch(cmd.toLowerCase()) {
+  case "device":
+    player
+      .playOnDevice(...args)
+      .then(() => console.log('Playback started successfully'))
+      .catch((err) => { console.error(err); });
+    break;
+  case "volume":
+    player
+      .setVolume(...args)
+      .then(() => console.log('Volume changed successfully'))
+      .catch((err) => { console.error(err); });
+    break;
+  case "next":
+    player
+      .skipNext(...args)
+      .then(() => console.log('Playback skipped successfully'))
+      .catch((err) => { console.error(err); });
+    break;
+  case "prev":
+    player
+      .skipPrevious(...args)
+      .then(() => console.log('Playback rewound successfully'))
+      .catch((err) => { console.error(err); });
+    break;
+  case "pause":
+    player
+      .pause(...args)
+      .then(() => console.log('Playback paused successfully'))
+      .catch((err) => { console.error(err); });
+    break;
+  case "play":
+    player
+      .play(...args)
+      .then(() => console.log('Playback started successfully'))
+      .catch((err) => { console.error(err); });
+    break;
+  default:
+    console.log("available commands: {device, volume, next, prev, pause, play}");
+}
